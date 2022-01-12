6d796a99d056c3243100f1e8d0f75eb6400d1aea
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-01-12 11:33:21 -0500

    commit tree b2a5a89ef411a09fb91b16cfa3fca26746ba174c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :title, exclusion: { in: %w("True Facts") }
+    validate :title_is_clickbait
+
+    #Custom Validations
+    def title_is_clickbait
+        if title == "True Facts"
+            errors.add(:title, "that title is prohibited")
+        end
+    end
+
 end
+
+
+# All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+# Post category is either Fiction or Non-Fiction. This step requires an inclusion validator, which was not outlined in the lesson. You'll need to refer to the Rails guide to look up how to use it.
